d9ace271cda0ba414ea42bb2f01c3408a2b2fc36
starcout <epreyesuclv@gmail.com>
2021-09-27 17:42:40 -0400

    merging

---
+++ src/fetchData/floweQuerys.js
@@ -0,0 +1,129 @@
+const { Pool } = require("pg")
+const { Flower } = require("../models/Flowers")
+
+const { FlowerOwner } = require("../models/FlowerOwner")
+const { UserBack } = require("../models/userBack")
+require("dotenv").config
+const { DATABASE, PASSWORD, USER, PORT } = process.env
+
+
+const pool = new Pool({
+    host: "localhost",
+    user: USER,
+    password: PASSWORD,
+    port: PORT,
+    database: DATABASE
+})
+
+
+
+//get all vendor given a Flower
+async function getVendors(flowerName) {
+
+    const response = await FlowerOwner.findAll({
+        where: {
+            flowerName: flowerName
+        }
+    }).catch(handlercatch)
+    console.log("flowersQuerys-getVendors", flowerName)
+    let vendors = []
+
+    for (let vendor of response) {
+        vendors.push(await UserBack.findByPk(vendor.ownerName))
+    }
+
+    return vendors
+}
+
+async function getAllVendor() {
+
+    const response = await UserBack.findAll()
+    //console.log("flowersQuerys ",response)
+
+    return response
+}
+
+
+async function insertFlower(name, region, color) {
+
+    //write your query insert here
+    const response = await Flower.create({ name: name, region: region, color: color })
+
+    console.log("flowersQuerys- insertFlower", response)
+
+    return response
+}
+
+
+
+//insert new one vendor's Flower
+
+async function insertFlowerOnwer(name, owner) {
+
+    const response = await FlowerOwner.create({ flowerName: name, ownerName: owner })
+
+    return response
+}
+
+
+
+async function selectFlowerOwner(id, email) {
+
+    //write your query select here
+    const response = await FlowerOwner.findAll({
+        where: {
+            flowerName: id,
+            ownerName: email
+        }
+    })
+    return response
+}
+
+
+
+//delete a one vendor's Flower
+
+async function deleteFlower(idFlower, owner) {
+    //write your query delete here
+    await FlowerOwner.destroy({
+        where: {
+            flowerName: idFlower,
+            ownerName: owner
+        }
+    })
+    return response
+}
+
+
+
+async function getAllflowers() {
+
+    return await Flower.findAll().catch(handlercatch)
+
+    //need fix
+}
+
+
+
+async function selectflower(id) {
+
+    return await Flower.findByPk(id).catch(handlercatch)
+
+}
+
+
+
+function handlercatch(err) {
+    console.log("flowersQuerys - handlercatch ", err)
+}
+
+module.exports = {
+    deleteFlower,
+    getAllVendor,
+    getVendors,
+    selectFlowerOwner,
+    insertFlower,
+    insertFlowerOnwer,
+    selectflower,
+    getAllflowers
+}
---
+++ src/fetchData/requestData.js
@@ -0,0 +1,82 @@
+const { default: axios } = require("axios");
+const { getAllVendor, getVendors } = require("./floweQuerys")
+const { cleanInsertAll } = require("./cleanOperations")
+async function buyFlowerNode(name, amount, address) {
+
+    const vendors = await getVendors(name)
+    let sell_it = false
+    let data
+    let status
+
+    axios.defaults.timeout = 3000
+    console.log("requestData-buyFlower ", vendors)
+    for (let v of vendors) {
+        if (sell_it)
+            break;
+        //I supose that all vendors, have enough flower to sell,this features will change in the future
+        console.log("requestData - inside for", v.endPoint)
+        const options = {
+            method: 'POST',
+            url: `${v.endPoint}buy`,
+            headers: { 'Content-Type': 'application/json' },
+            data: { name: name, amount: amount, address: address, token:"sjadfhjkqshf" }
+        };
+        axios.defaults.timeout = 3000
+        await axios.request(options).then(function (response) {
+            status = response.status
+            data = "It will delivers you the flowers soon"
+            sell_it = true
+            //console.log(response)
+        }).catch(function (error) {
+            data = error
+                        //console.log("token ",error.response)
+        });
+    }
+
+
+    if (!sell_it)
+        status = 408
+    console.log("requestData - buyFlower ",data)
+    return {
+        status: status,
+        data: data
+    }
+}
+
+async function fetchFromAllNodes() {
+
+    const vendors = await getAllVendor()
+    console.log("requestData - fetchFromAllNodes",vendors)
+    for (let v of vendors) {
+        //console.log("requestData - fetchFromAllNodes",v)
+        const endPoint = v.endPoint
+
+        const options = {
+            method: 'GET',
+            url: `${endPoint}flowers`,
+            headers: { 'Content-Type': 'application/json' },
+        };
+
+        axios.defaults.timeout = 10000
+        await axios.request(options).then(async function (response) {
+            status = response.status
+            data = response.data
+            console.log("requestData - fetchFromAllNodes - axios request response",data)
+            await cleanInsertAll(data, v.email)
+          
+        }).catch(function (error) {
+            data = error
+            //console.log("token ",error.response)
+        });
+
+        console.log("requestData-fetchFromAllNodes", data)
+    }
+
+
+}
+
+
+module.exports = {
+    buyFlowerNode,
+    fetchFromAllNodes
+}
