fb409998ce1d231389de72cc4627f6f990bfa985
starcout <epreyesuclv@gmail.com>
2021-09-28 15:02:18 -0400

    database_connection fixed

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,12 +1,12 @@
 const { Sequelize} = require("sequelize")
 
 require("dotenv").config()
-const { USER, PASSWORD, PORT, DATABASE } = process.env
+const { USER, PASSWORD, PORT, DATABASE ,HOST} = process.env
 
 
 const sequelize = new Sequelize(DATABASE, USER, PASSWORD, {
 
-    host: 'localhost',
+    host: HOST,
     dialect: 'postgres'
 });
 
